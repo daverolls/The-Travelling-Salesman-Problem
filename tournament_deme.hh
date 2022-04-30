8647cd792804f9eb7613124c024d43291546cbf4
PhantomData87 <56553338+PhantomData87@users.noreply.github.com>
2022-04-29 20:42:10 -0700

    Delete tournament_deme.hh

--- tournament_deme.hh
+++
@@ -1,9 +0,0 @@
-#include "deme.hh"
-
-//////////////////////////////////////////////////////////////////////////////
-class TournamentDeme : public deme{
- public:
-  Chromosome* select_parent() override;
- private:
-  int TournamentDeme::power_of_two();
-}
